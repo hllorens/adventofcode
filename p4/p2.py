954711f222e93db3a76c1f3b9002e47b3f2131db
Hector Llorens <hectorlm1983@gmail.com>
2022-12-16 17:38:39 +0100

    advent dec 16

---
+++ p4/p2.py
@@ -0,0 +1,29 @@
+#/usr/bin/python3
+
+
+
+file1 = open('input.txt', 'r')
+Lines = file1.readlines()
+
+count = 0
+elements=[]
+full_overlap=0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    line_clean=line.strip()
+    #print("Line{}: {}".format(count, line_clean))
+    if (line_clean == ""):
+        continue
+    else:
+        line_arr=line_clean.split(",")
+        range1=line_arr[0].split("-")
+        range2=line_arr[1].split("-")
+        if (int(range1[0])>=int(range2[0]) and int(range2[1])>=int(range1[0])) or (int(range2[0])>=int(range1[0]) and int(range1[1])>=int(range2[0])):
+            full_overlap+=1
+            print(f"{line_clean} full_overlap={full_overlap}")
+    if count == -1:
+        break
+
+
+print(f"Total {full_overlap}")
